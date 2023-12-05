59094124d13fe193b914dfd55ad8ff0b26bb9018
exec zsh <wind10cloud@gmail.com>
2023-12-05 11:39:30 +0900

    add models

---
+++ src/main/java/analix/DHIT/model/Team.java
@@ -0,0 +1,56 @@
+package analix.DHIT.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="team")
+public class Team implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="team_id")
+    private int teamId;
+
+    @Column(length=30)
+    private String name;
+
+    public Team() {
+    }
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<Assignment> assignments;
+
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Assignment> getAssignments() {
+        return assignments;
+    }
+
+    public void setAssignments(List<Assignment> assignments) {
+        this.assignments = assignments;
+    }
+}

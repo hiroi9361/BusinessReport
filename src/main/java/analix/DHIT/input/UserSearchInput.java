bd0f2572e8bca6a2f84735d6b0dbe8d59cb0cb3c
exec zsh <wind10cloud@gmail.com>
2023-12-18 15:52:06 +0900

    changed styles

---
+++ src/main/java/analix/DHIT/input/UserSearchInput.java
@@ -0,0 +1,24 @@
+package analix.DHIT.input;
+
+public class UserSearchInput {
+
+    private String searchKeyword;
+
+    private String searchWords;
+
+    public String getSearchKeyword() {
+        return searchKeyword;
+    }
+
+    public void setSearchKeyword(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+    }
+
+    public String getSearchWords() {
+        return searchWords;
+    }
+
+    public void setSearchWords(String searchWords) {
+        this.searchWords = searchWords;
+    }
+}
